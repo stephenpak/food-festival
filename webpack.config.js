992c4e14c50d7abb146c7a7b933037fc9f853866
Stephen Pak <stephenjpak@gmail.com>
2022-07-17 17:07:29 -0700

    7-11-22

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require('path');
 const webpack = require('webpack');
 module.exports = {
@@ -10,6 +12,9 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
     }),
   ],
   mode: 'development',
